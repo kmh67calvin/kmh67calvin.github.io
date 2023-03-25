914a18769af8f73a57ec2d45cc028857372af7e9
Kenny Howes <kmh67@calvin.edu>
2023-03-24 22:01:47 -0400

    stuff

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const input = document.querySelector(".input");
 const editor = document.querySelector(".editor");
+const squares = document.querySelectorAll(".square");
+
+setInputToTape = () => {
+    for(let i = 0; i < input.value.length; i++) {
+        squares[i].innerHTML = input.value[i];
+    }
+}
 
 interpretEditor = () => {
-    let editorValue = editor.value;
+    alert(editor.value);
+    // let lines = editor.value.split("\n");
+    // lines.forEach(line => alert(line));
+}
 
-    alert(editorValue);
+compile = () => {
+    setInputToTape();
+    interpretEditor();
 }
